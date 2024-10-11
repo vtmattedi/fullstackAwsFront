2c81c18b70af2b97387e131b5542db793c798a62
= <=>
2024-10-11 11:54:36 -0300

    class of 11.10

--- src/Components/Dashboard.tsx
+++ src/Components/Dashboard.tsx
@@ -14,6 +14,8 @@ const Dashboard: React.FC<DashboardProps> = ({ handleLoading }) => {
     const { handleLogout } = useAuth();
     const [disableRefresh, setDisableRefresh] = React.useState(false);
     const [myInfo, setMyInfo] = React.useState<{ user: string, email: string, id: number } | undefined>(undefined);
+    const [myPosts, setPosts] = React.useState<{ title: string, content: string, created_time: string, postid:number }[]>([]);
+    
     const logout = () => {
         handleLogout();
         axiosLogout.delete('/logout').then((response) => {
@@ -62,6 +64,14 @@ const Dashboard: React.FC<DashboardProps> = ({ handleLoading }) => {
 
     useEffect(() => {
         updateData();
+        axios.get('/posts/1').then((response) => {
+            console.log(response.data);
+            const {posts} = response.data;
+            setPosts(posts);
+            console.log(posts);
+        }).catch((error) => {	
+            console.log(error);	
+        });
     }, []);
     return (
         <div>
@@ -72,13 +82,28 @@ const Dashboard: React.FC<DashboardProps> = ({ handleLoading }) => {
                 <input type='text' placeholder='User Name'/>
                 <button>Search</button>
             </div>
-            <div>
+            {/* <div>
                 <Post title='sample' text='lorem ipsum' onDelete={() => {handleDelete(2)}}/>
+            </div> */}
+            <div style={{maxHeight: '50vh', overflowY: 'scroll', overflowX:'hidden'}}>
+            {
+                myPosts.map((post, index) => {
+                    return <Post key={index} title={post.title} text={post.content} onDelete={() => {handleDelete(post.postid)}}/>
+                })
+            }
             </div>
             <button onClick={logout}>log out</button>
             <button onClick={updateData} disabled={disableRefresh}>refresh data</button>
             <button onClick={getOthers}>get others</button>
-            <button onClick={getOthers}>post</button>
+            <button onClick={()=>
+                {
+                    axios.get('/posts/1',{data: {targetid:1}}).then((response) => {
+                        console.log(response.data);
+                    }).catch((error) => {
+                        console.log(error);
+                    });
+                }
+            }>post</button>
         </div>
     );
 };
